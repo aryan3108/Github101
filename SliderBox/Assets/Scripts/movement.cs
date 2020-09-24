37e980f1a02bff75187c7835457cb21bb0ddcf1c
Sumit kumar <56474834+Sumit-Kumar7796@users.noreply.github.com>
2020-09-24 16:05:56 +0530

    Added left right movements to the player
    Left right movement and changed the player orientation and plateform size and position

--- SliderBox/Assets/Scripts/movement.cs
+++ SliderBox/Assets/Scripts/movement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	private float speed = 2.0f;
+	public GameObject character;
+	void FixedUpdate()
     {
-        
+		
     }
+	void Update()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		
+	}
+
 }
