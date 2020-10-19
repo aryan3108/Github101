890ee45de8b408099d1ca818bce9835ef6929919
Sumit kumar <56474834+Sumit-Kumar7796@users.noreply.github.com>
2020-10-19 08:50:42 +0530

    Update player movement scrip, added arrow key functionality

--- SliderBox/Assets/Scripts/movement.cs
+++ SliderBox/Assets/Scripts/movement.cs
@@ -3,19 +3,20 @@
 public class movement : MonoBehaviour
 {
 	public Rigidbody rb;
-	public float forceforward = 1500f, forceSideway = 1200f;
+	public float forceforward = 800f, forceSideway = 350f;
 
 	void FixedUpdate()
     {
 		rb.AddForce(0, 0, -forceforward * Time.deltaTime);
-		if(Input.GetKey("d"))
+		if(Input.GetKey("d")|Input.GetKey("right"))
         {
 			rb.AddForce(-forceSideway * Time.deltaTime, 0, 0);
         }
-		if (Input.GetKey("a"))
+		if(Input.GetKey("a")|Input.GetKey("left"))
 		{
 			rb.AddForce(forceSideway * Time.deltaTime, 0, 0);
 		}
+		forceforward++;
 	}
 
 
