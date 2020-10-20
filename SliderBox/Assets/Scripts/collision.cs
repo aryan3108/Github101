0bb27f5d93b9827b85f739289a68f492f949bab7
Sumit kumar <56474834+Sumit-Kumar7796@users.noreply.github.com>
2020-10-20 19:46:41 +0530

    Added collision script to detect collision

---
+++ SliderBox/Assets/Scripts/collision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+   void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            print("we hit an obstacle");
+        }
+    }
+}
